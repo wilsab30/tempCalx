ac85f4dd3efc06c3bea0fa0a23502dc6d337cbbb
Sabrina Wilbert <sabrinawilbert@gmail.com>
2017-01-03 12:37:15 -0800

    Initial Commit

---
+++ TempCal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TempCal
+//
+//  Created by Sabrina Wilbert on 1/3/17.
+//  Copyright © 2017 Sabrina Wilbert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
